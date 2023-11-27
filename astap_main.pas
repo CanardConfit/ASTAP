a8961d58c9978b7a4065506b2232e403a1f17bd2
Han59 <han.k@hnsky.org>
2023-11-27 22:42:30 +0100

    'v2023.11.27'

--- astap_main.pas
+++ astap_main.pas
@@ -62,7 +62,7 @@ uses
   IniFiles;{for saving and loading settings}
 
 const
-  astap_version='2023.11.17';  //  astap_version := {$I %DATE%} + ' ' + {$I %TIME%});
+  astap_version='2023.11.27';  //  astap_version := {$I %DATE%} + ' ' + {$I %TIME%});
 
 type
   { Tmainwindow }
