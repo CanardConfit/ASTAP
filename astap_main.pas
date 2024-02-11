f13d9d04342864fb7402d58e279da70bf06c6d74
Han59 <han.k@hnsky.org>
2024-02-11 16:37:01 +0100

    'v2024.02.11'

--- astap_main.pas
+++ astap_main.pas
@@ -62,7 +62,7 @@ uses
   IniFiles;{for saving and loading settings}
 
 const
-  astap_version='2024.02.08';  //  astap_version := {$I %DATE%} + ' ' + {$I %TIME%});
+  astap_version='2024.02.11';  //  astap_version := {$I %DATE%} + ' ' + {$I %TIME%});
 
 type
   { Tmainwindow }
