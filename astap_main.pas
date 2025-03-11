a94aa4b940a12f7e4b81187593351373ae78a7b5
Han59 <han.k@hnsky.org>
2025-03-11 16:23:28 +0100

    'v2025.03.11'

--- astap_main.pas
+++ astap_main.pas
@@ -58,7 +58,7 @@ uses
   IniFiles;{for saving and loading settings}
 
 const
-  astap_version='2025.03.07';  //  astap_version := {$I %DATE%} + ' ' + {$I %TIME%});
+  astap_version='2025.03.11';  //  astap_version := {$I %DATE%} + ' ' + {$I %TIME%});
 type
   tshapes = record //a shape and it positions
               shape : Tshape;
